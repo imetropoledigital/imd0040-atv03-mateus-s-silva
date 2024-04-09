bd3fe02e83377db53906452f0d25086651eee0a3
mateus-s-silva <mattahyah@gmail.com>
2024-04-09 13:20:00 -0300

    First commit

---
+++ src/main/java/br/ufrn/imd/Person.java
@@ -0,0 +1,18 @@
+package br.ufrn.imd;
+
+public class Person {
+    public  String name;
+    public String hometown;
+
+    public Person(){
+
+    }
+
+    public String hometown(){
+        return this.hometown;
+    }
+
+    public String name(){
+        return this.name;
+    }
+}
